0d19f09f471b4bcaf616a54c0f2eb0be36c54738
PranamDoshi <pranam.rpdoshi@gmail.com>
2023-07-11 23:22:59 +0530

    Updated Saving Password System

--- Saving Passwords/StorePasswords.py
+++ Saving Passwords/StorePasswords.py
@@ -1,68 +1,185 @@
 import pickle
-from PasswordDictionaryClass import SavedPasswords
+from collections import defaultdict
+from datetime import datetime
+from pathlib import Path
+import json
+from pprint import pprint, PrettyPrinter
+
+prettyPrinterConfig = PrettyPrinter(indent=4)
+def print_data(obj):
+    prettyPrinterConfig.pprint(obj)
+
+
+class SavedPasswords:
+    passwords = {}
+
+    @staticmethod
+    def savePassword(title: str, password: str, **kwargs):
+        if SavedPasswords.checkIfTitleExists(title):
+            SavedPasswords.passwords[title]['password'] = password
+            SavedPasswords.passwords[title]['password_history'].append(
+                f"{datetime.now().strftime('%y-%m-%d')}_{password}"
+            )
+
+        else:
+            SavedPasswords.passwords[title] = {
+                'password_history': [f"{datetime.now().strftime('%y-%m-%d')}_{password}"],
+                'password': password,
+                'title_history': [f"{datetime.now().strftime('%y-%m-%d')}_{title}"]
+            }
+
+            # Add other parameters if received any.
+            kwargs.pop('password', None)
+            SavedPasswords.passwords[title].update(kwargs)
+
+    @staticmethod
+    def getPassword(title: str)-> str:
+        return SavedPasswords.passwords[title]
+
+    @staticmethod
+    def getPasswordTitles()-> list:
+        return list(SavedPasswords.passwords.keys())
+
+    @staticmethod
+    def delPassword(title: str):
+        SavedPasswords.passwords.pop(title)
+
+    @staticmethod
+    def updatePasswordTitle(currentTitle: str, newTitle: str):
+        SavedPasswords.passwords[newTitle] = SavedPasswords.passwords[currentTitle]
+        SavedPasswords.passwords[newTitle]['title_history'].append(
+            f"{datetime.now().strftime('%y-%m-%d')}_{newTitle}"
+        )
+
+        SavedPasswords.passwords.pop(currentTitle, None)
+
+    @staticmethod
+    def checkIfTitleExists(title: str)-> bool:
+        if SavedPasswords.passwords.get(title, False):
+            return True
+
+        return False
+
+    @staticmethod
+    def dumpToPickle(filepath: str):
+        with open(filepath, 'wb') as file:
+            pickle.dump(SavedPasswords.passwords, file)
+
+    @staticmethod
+    def readFromPickle(filepath: str):
+        with open(filepath, 'rb') as file:
+            SavedPasswords.passwords = pickle.load(file)
+
+    @staticmethod
+    def dumpToJSON():
+        print(json.dumps(SavedPasswords.passwords, indent=3))
+
+    @staticmethod
+    def readFromJSON(filepath: str):
+        with open(filepath, 'r') as file:
+            SavedPasswords.passwords = json.load(file)
+
 
 #Globalvariables
-dictPickleFile = 'E:\Work\Git Repos\Saved.pkl'
+dictPickleFile = f"{Path(Path(__file__).parent).as_posix()}/Saved.pkl"
+
+def savePassword():
+    """ Save/Update Password """
+    title = input("What is the title? :- ").strip()
+    password = input("What is the password? :- ").strip()
+    email = input("What is the associated email? (If any) :- ")
+
+    if title not in {'', ' '}:
+        SavedPasswords.savePassword(title, password, email=email)
+        SavedPasswords.dumpToPickle(dictPickleFile)
+
+def viewPassword():
+    """ View Password """
+    titles = SavedPasswords.getPasswordTitles()
+    for case, title in enumerate(titles):
+        print(f"{case} -- {title}", end='\n')
+    inp = int(input("Choose the number beside the password you want to update:- "))
+
+    if inp < len(titles):
+        print_data(SavedPasswords.getPassword(titles[inp]))
+
+    else:
+        print(f"Title not found.\n")
+
+def viewTitles():
+    """ View Password Titles """
+    titles = SavedPasswords.getPasswordTitles()
+    print_data({case: title for case, title in enumerate(titles)})
+
+def deletePassword():
+    """ Delete an existing password """
+    titles = SavedPasswords.getPasswordTitles()
+    for case, title in enumerate(titles):
+        print(f"{case} -- {title}", end='\n')
+
+    inp = int(input("Choose the number beside the password you want to update:- "))
+    if inp < len(titles):
+        SavedPasswords.delPassword(titles[inp])
+        SavedPasswords.dumpToPickle(dictPickleFile)
+
+    else:
+        print("No password exist for given value. Try again!")
+        deletePassword()
+
+def updatePasswordTitle():
+    """ Update an existing password title """
+    titles = SavedPasswords.getPasswordTitles()
+    for case, title in enumerate(titles):
+        print(f"{case} -- {title}", end='\n')
+
+    inp = int(input("Choose the number beside the password you want to update:- "))
+    if inp < len(titles):    
+        newTitle = input("What is the new title? :- ").strip()
+
+        SavedPasswords.updatePasswordTitle(titles[inp], newTitle)
+        SavedPasswords.dumpToPickle(dictPickleFile)
+
+    else:
+        print("Not an actual title. Try again!")
+        updatePasswordTitle()
+
+def allDone():
+    """ All Done! """
+    print(f"\nIt was Great to see you. Come back soon!")
+    exit()
+
+case_to_function = {
+    1: savePassword,
+    2: viewPassword,
+    3: viewTitles,
+    4: deletePassword,
+    5: updatePasswordTitle,
+    6: allDone
+}
+
+def getUserChoice()-> int:
+    for case, function in case_to_function.items():
+        print(f"{case} -{function.__doc__}", end='\n')
+
+    inp = int(input("Choose one of the option from above:- "))
+
+    return inp
 
 if __name__ == "__main__":
-    # passwords = SavedPasswords()
-    with open(dictPickleFile, 'rb') as pklFile:
-        passwords = pickle.load(pklFile)
-    
-    while True:
-        inp = int(input("1 - Save Password \
-                        \n2 - View Password \
-                        \n3 - View Categories \
-                        \n4 - Change an existing Passowrd Category name \
-                        \n5 - Delete categories (Provide comma separated indexs.) \
-                        \n6 - Update an existing category \
-                        \n0 - I'm Done\n"))
+    # SavedPasswords.readFromJSON("Saved.json")
+    # SavedPasswords.dumpToPickle(dictPickleFile)
+    SavedPasswords.readFromPickle(dictPickleFile)
+    # SavedPasswords.dumpToJSON()
 
-        if inp == 1:
-            name = input("Enter Category Name:")
-            
-            # Checks if the category already exists.
-            if passwords.checkCategoryIfExists(name):
-                print("Please use the existing category!")
+    try:
 
-            if name not in ['0', None, '', '\t', '\n']:
-                password = input("Enter Password:")
-                passwords.savePassword(name, password)
-        elif inp == 2:
-            categories = passwords.getCategories()
-            if categories:
-                for idx, catg in enumerate(categories):
-                    # print("%d - %s" % (idx, catg))
-                    print(f"{idx} - {catg}")
-                
-                inp = int(input("Which category: "))
-                if inp >= 0 and inp < len(categories):
-                    passwords.viewPassword(categories[inp])
-                else:
-                    print("Select a Valid Option")
-            else:
-                print("No passwords Saved")
-        elif inp == 3:
-            passwords.viewCategories()
-        elif inp == 4:
-            oldCategory = input("Enter the old category")
-            newCategory = input("Enter the new category")
+        while True:
+            inp = getUserChoice()
 
-            passwords.ChangeCategories(oldCategory, newCategory)
-        elif inp == 5:
-            categories = passwords.getCategories()
-            inpIdxs = list(map(int, input().split(',')))
+            match inp:
 
-            for idx in inpIdxs:
-                passwords.delPassword(categories[inpIdxs])
-        elif inp == 6:
-            name = input("Enter the existing category name:")
+                case k:
+                    case_to_function[k]()
 
-            if name not in ['0', None, '', '\t', '\n']:
-                password = input("Enter Password:")
-                passwords.savePassword(name, password)
-        else:
-            break
-     
-    with open(dictPickleFile, 'wb') as pklFile:
-        pickle.dump(passwords, pklFile)
+    except KeyboardInterrupt:
+        case_to_function[6]()
